07aadd3c49172830ecd170a88c96d4f3dd1e981b
fatima zahra <fatimazahraessadiki30@gmail.com>
2026-03-09 10:18:37 +0000

    creation de l'entité JPA Product

---
+++ tp2_spring/src/main/java/org/example/tp2_spring/entities/Product.java
@@ -0,0 +1,29 @@
+package org.example.tp2_spring.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class Product {
+    @GeneratedValue
+    private Long id;
+    @NotEmpty
+    @Size(min = 3 ,max=50)
+    private String name;
+    @Min(0)
+    private  double price;
+    @Min(1)
+    private double quantity;
+
+}
